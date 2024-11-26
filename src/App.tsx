954ed0aa9456a9368f0810d5271d3d498c45b8a1
Hushani Weerasinghe <hushanid@gmail.com>
2024-11-26 14:58:47 +0530

    fix: remove not use

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PolygonCanvas from './componets/PolygonCanvas';
 import PolygonDrawingTool from './componets/PolygonTool';
 function App() {
   return (
@@ -7,9 +6,8 @@ function App() {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <h1 style={{ textAlign: 'center' }}>Polygon Drawer</h1>
       </div>
-      
+
       <PolygonDrawingTool />
-      {/* <PolygonCanvas /> */}
     </div>
   );
 }
